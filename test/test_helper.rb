24419060853316cadc30ed768eba462b3b7e97ac
James Mead <james@floehopper.org>
2014-01-10 11:57:25 +0000

    Attempt to reproduce a Mocha issue.
    See https://github.com/freerange/mocha/issues/181.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'rubygems'
+# gem 'test-unit'
+require 'test/unit'
+require 'mocha/setup'
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
