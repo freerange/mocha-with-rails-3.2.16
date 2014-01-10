bb6e97a3ff92fba8dcd785e16ba8c1d1d968e64d
James Mead <james@floehopper.org>
2014-01-10 11:47:07 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MochaWithRails3216::Application.config.secret_token = '053ae2e99367fbc670e356b378d016d273d62397a345923c3797da2af49c53ef2dc7b4463e9c5ddcdbf0b59d2e6ae32e2aa1e8c7a3615c2fbb5be097c3f1559e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MochaWithRails3216::Application.config.session_store :cookie_store, key: '_mocha-with-rails-3.2.16_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MochaWithRails3216::Application.config.session_store :active_record_store
